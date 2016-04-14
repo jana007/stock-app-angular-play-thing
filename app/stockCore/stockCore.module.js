5ca7c33c2157ece8a4b2e241d987213acb3d2cdc
Jana McKinnon <JMcKinnon@campus.fsu.edu>
2016-04-14 12:46:01 -0400

    First commit

---
+++ app/stockCore/stockCore.module.js
@@ -0,0 +1,14 @@
+angular
+	.module("app.stockCore", [
+		// Angular Modules
+		// "ngRoute",
+
+		// "3rd Party Module",
+		// "breeze.angular",
+		// "ui.bootstrap",
+
+		// "custom cross app modules",
+		// "crossapp.logger",
+		// "crossapp.exceptions",
+
+	]);
---
+++ app/stockList/stockList.module.js
@@ -0,0 +1,2 @@
+angular
+	.module("app.stockList", []); // instantiate dependency first!!
